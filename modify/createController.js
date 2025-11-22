48f2fdf0bd029fe0ff2d63c526dc1c2610bd9f34
MobKill3r2006 <supermanwouter@gmail.com>
2025-11-22 07:34:48 +0100

    added data.db to gitignore and added option for custom IDs

--- modify/createController.js
+++ modify/createController.js
@@ -4,15 +4,24 @@ const Link = defineLink(sequelize);
 
 exports.register = async (req, res) => {
   try {
-    const { url } = req.body;
+    const { url, id } = req.body;
+
+    // Generate ID if none given
+    const shortId = id || nanoid(6);
+
     const link = await Link.create({
       url: url,
+      id: shortId,
     });
     res.status(201).json({
       success: true,
       link: { id: link.id, url: link.url, visited: link.visited },
     });
   } catch (err) {
-    res.status(500).json({ success: false, error: err.message });
+    if (err.name === "SequelizeUniqueConstraintError") {
+      errorMsg = "Custom ID already exists. Please choose a different ID.";
+      res.status(400).json({ success: false, error: errorMsg, name: err.name });
+    }
+    res.status(500).json({ success: false, error: err.message, name: err.name });
   }
 };
